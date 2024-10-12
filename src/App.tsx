c3652fa5159ee04b1b887b41c071552295426efb
PhilSchoe <p.schoemacker@gmx.de>
2024-10-12 17:23:11 +0200

    Add basic page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen bg-sky-950">
+      <div className="flex justify-center container relative top-16 rounded shadow-2xl bg-sky-700 mx-auto px-4 py-4">
+        <div className="text-4xl font-bold text-gray-300">
+          Holiday Planner AI
+        </div>
+      </div>
     </div>
   );
 }
