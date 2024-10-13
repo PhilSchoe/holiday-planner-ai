dbf4f74b17f85aebcdb7c33fa8974a6af7b15ed9
PhilSchoe <p.schoemacker@gmx.de>
2024-10-13 10:40:23 +0200

    Add HolidayInfoInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import HolidayInfoInput from "./HolidayInfoInput";
+
 function App() {
   return (
     <div className="min-h-screen bg-sky-950">
-      <div className="flex justify-center container relative top-16 rounded shadow-2xl bg-sky-700 mx-auto px-4 py-4">
+      <div className="flex flex-col items-center justify-center container relative top-16 rounded shadow-2xl bg-sky-700 mx-auto px-4 py-4">
         <div className="text-4xl font-bold text-gray-300">
           Holiday Planner AI
         </div>
+
+        <HolidayInfoInput />
       </div>
     </div>
   );
---
+++ src/HolidayInfoInput.tsx
@@ -0,0 +1,8 @@
+export default function holidayInfoInput() {
+  return (
+    <div className="flex justify-between py-4 gap-4">
+      <div>Icon</div>
+      <div>Location</div>
+    </div>
+  );
+}
