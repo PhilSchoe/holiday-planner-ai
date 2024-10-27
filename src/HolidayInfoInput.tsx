b158821fd9ea4b6a7f0ebcff02bc9e5bc8ee3085
PhilSchoe <p.schoemacker@gmx.de>
2024-10-27 20:02:29 +0100

    Add submit button to initiate planning and make inputs controlled

--- src/HolidayInfoInput.tsx
+++ src/HolidayInfoInput.tsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import InputPanel from "./InputPanel";
 
 export default function holidayInfoInput() {
+  const [location, setLocation] = useState<string>("");
+  const [duration, setDuration] = useState<string>("");
+
+  function handleSubmit() {
+    console.log("location: ", location);
+    console.log("duration: ", duration);
+  }
+
   return (
-    <div>
-      <InputPanel
-        title="Location"
-        subtitle="Where do you want to go?"
-        iconKey="map-pin"
-      />
-      <InputPanel
-        title="Time"
-        subtitle="How long are you going to stay?"
-        iconKey="clock"
-      />
-    </div>
+    <>
+      <div>
+        <InputPanel
+          title="Location"
+          subtitle="Where do you want to go?"
+          iconKey="map-pin"
+          onInputChage={setLocation}
+        />
+        <InputPanel
+          title="Time"
+          subtitle="How long are you going to stay?"
+          iconKey="clock"
+          onInputChage={setDuration}
+        />
+      </div>
+      <button type="button" onClick={handleSubmit}>
+        Plan Now!
+      </button>
+    </>
   );
 }
--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -4,12 +4,14 @@ interface InputPanelProps {
   title: string;
   subtitle: string;
   iconKey: string;
+  onInputChage: (value: string) => void;
 }
 
 export default function inputPanel({
   title,
   subtitle,
   iconKey,
+  onInputChage,
 }: InputPanelProps) {
   return (
     <div className="flex flex-nowrap items-center mb-2 py-4 gap-4">
@@ -21,6 +23,7 @@ export default function inputPanel({
         <input
           type="text"
           className="bg-sky-700 border border-gray-300 focus:outline-none focus:border-sky-950 focus:ring-1 focus:ring-sky-950 text-gray-300 rounded-lg p-0.5 pl-2"
+          onChange={(e) => onInputChage(e.target.value)}
         />
       </div>
     </div>
