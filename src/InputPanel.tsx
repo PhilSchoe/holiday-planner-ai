f73b939f904f8820265150c1f8899adefad15009
PhilSchoe <p.schoemacker@gmx.de>
2024-10-27 16:52:42 +0100

    Add styling to input field

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -18,7 +18,10 @@ export default function inputPanel({
       <div>
         <div className="text-gray-300">{title}</div>
         <div className="text-gray-300">{subtitle}</div>
-        <input></input>
+        <input
+          type="text"
+          className="bg-sky-700 border border-gray-300 focus:outline-none focus:border-sky-950 focus:ring-1 focus:ring-sky-950 text-gray-300 rounded-lg p-0.5 pl-2"
+        />
       </div>
     </div>
   );
