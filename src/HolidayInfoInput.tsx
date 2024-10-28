b6a2b053ee203ccdf382f9c20ed66af7fc6e49cf
PhilSchoe <p.schoemacker@gmx.de>
2024-10-28 20:23:23 +0100

    Add styling to submit button

--- src/HolidayInfoInput.tsx
+++ src/HolidayInfoInput.tsx
@@ -12,7 +12,7 @@ export default function holidayInfoInput() {
 
   return (
     <>
-      <div>
+      <div className="mb-6">
         <InputPanel
           title="Location"
           subtitle="Where do you want to go?"
@@ -26,7 +26,12 @@ export default function holidayInfoInput() {
           onInputChage={setDuration}
         />
       </div>
-      <button type="button" onClick={handleSubmit}>
+      <button
+        disabled={!location || !duration}
+        type="button"
+        className="bg-sky-950 text-gray-300 rounded-lg px-5 py-2 border-2 border-transparent transition-all duration-300 hover:border-gray-300 active:bg-sky-800 disabled:opacity-50 disabled:border-transparent disabled:cursor-not-allowed disabled:active:bg-sky-950"
+        onClick={handleSubmit}
+      >
         Plan Now!
       </button>
     </>
