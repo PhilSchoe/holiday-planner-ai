31434cf5c7667b49c7406c8e2a3fb3c284a08951
PhilSchoe <p.schoemacker@gmx.de>
2025-02-01 21:29:16 +0100

    Add transitions to Modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -6,11 +6,17 @@ interface ModalProps {
 export default function Modal({ open, children }: ModalProps) {
   return (
     <div
-      className={`fixed inset-0 flex items-start justify-center pt-32 ${
+      className={`fixed inset-0 flex items-start justify-center transition-colors pt-32 ${
         open ? "visible bg-black/30" : "invisible"
       }`}
     >
-      <div className="bg-white rounded shadow p-6">{children}</div>
+      <div
+        className={`bg-white rounded shadow p-6 transition-all ${
+          open ? "scale-100 opacity-100" : "scale-125 opacity-0"
+        }`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
