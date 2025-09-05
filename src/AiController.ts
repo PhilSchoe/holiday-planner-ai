e3cac37fbcb87098242874ce9b4b447fc0c10467
PhilSchoe <p.schoemacker@gmx.de>
2025-09-05 23:12:58 +0200

    Update to latest release of chrome built-in api
    - Update @types/dom-chromium-ai
    - LanguageModel no longer to be called from self
    - Adjust prompt

--- src/AiController.ts
+++ src/AiController.ts
@@ -1,29 +1,28 @@
 export async function checkAvailability(): Promise<boolean> {
-  if (!self.ai || !self.ai.languageModel) {
+  if (!("LanguageModel" in self)) {
     return false;
   }
 
-  const { available, defaultTemperature, defaultTopK, maxTopK } =
-    await self.ai.languageModel.capabilities();
-
-  if (available !== "no") {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
 
 export async function getHolidayPlanning(
   location: string,
   duration: string
 ): Promise<ReadableStream<string>> {
-  const session = await self.ai.languageModel.create({
-    systemPrompt:
-      "You work for a travel agency and are asked to plan a vacation. The client tells you the destination and duration of their stay. Always close your answer by wishing a nice trip",
+  const session = await LanguageModel.create({
+    initialPrompts: [
+      {
+        role: "system",
+        content:
+          "You work for a travel agency and are asked to plan a vacation. The client tells you the destination and duration of their stay. Always close your answer by wishing a nice trip",
+      },
+    ],
   });
 
   const stream = session.promptStreaming(
-    `I am going to ${location} for ${duration}. Please create a plan for me. I am interested in sightseeing, restaurants, cafés and other activities.`
+    `I am going to ${location} for ${duration}. Please create a plan for me. I am interested in sightseeing, restaurants, cafés and other activities. 
+    Please create the plan as markdown text with headlines, bullet points and emojis.`
   );
 
   return stream as unknown as ReadableStream<string>;
